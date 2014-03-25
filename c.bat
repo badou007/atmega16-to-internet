da2105afa98eb25962078a3aba49cdc1cfd6e8a0
Raoul Diop <badou007@users.noreply.github.com>
2014-03-25 15:46:43 +0100

    Create c.bat

---
+++ c.bat
@@ -0,0 +1,2 @@
+javac -d c:\tom\webapps\atmega\WEB-INF\classes -classpath c:\tom\webapps\atmega\WEB-INF\classes;c:\tom\common\lib\comm.jar -sourcepath c:\tom\webapps\atmega\WEB-INF\src c:\tom\webapps\atmega\WEB-INF\src\write\*.java
+
