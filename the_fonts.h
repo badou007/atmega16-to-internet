fda7fab0c81273e9bea781b445d6b7b6c4657529
Raoul Diop <badou007@users.noreply.github.com>
2014-03-25 15:35:18 +0100

    Create the_fonts.h

---
+++ the_fonts.h
@@ -0,0 +1,19 @@
+#define cs PORTB0
+#define wr PORTB1
+#define data PORTB2
+#define cs1 PORTB3
+#define cs2 PORTB4
+#define cs3 PORTB5
+
+
+
+#define ht1632_port PORTB
+#define ht1632_data_dir DDRB
+
+
+#define sys_en  0x02
+#define led_on 0x06
+#define master_rc 0b000101000
+#define slave_rc 0b100000
+#define commons 0b001010000// onprend ab=10 : voir data sheet
+#define pwm_16 0b101011110
